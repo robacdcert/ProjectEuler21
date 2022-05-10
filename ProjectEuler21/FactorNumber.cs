29c67dc335214fc79f0f7eb79908bf01a8f31dee
robacdcert <Robert.Griffin@IVP-LD6V08MG3>
2022-05-10 15:25:36 -0400

    Add project files.

---
+++ ProjectEuler21/FactorNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler21
+{
+    internal class FactorNumber
+    {
+        public int Number { get; set; } = 0;
+        public List<int> Divisors { get; set; } = new List<int>();
+        public int SumOfDivisors { get; set; } = 0;
+        public bool IsAmicable { get; set; } = false;
+        public FactorNumber? AmicablePair { get; set; } = null;
+
+        public void GetDivisors()
+        {
+            for (int i = 1; i <= Number / 2; i++)
+            {
+                if (Number % i == 0)
+                {
+                    Divisors.Add(i);
+                }
+            }
+            SumOfDivisors = Divisors.Sum();
+            FactorNumber possibleAmicablePair = new FactorNumber();
+            possibleAmicablePair.Number = SumOfDivisors;
+            for (int i = 1; i <= possibleAmicablePair.Number / 2; i++)
+            {
+                if (possibleAmicablePair.Number % i == 0)
+                {
+                    possibleAmicablePair.Divisors.Add(i);
+                }
+            }
+            possibleAmicablePair.SumOfDivisors = possibleAmicablePair.Divisors.Sum();
+            if ((Number == possibleAmicablePair.SumOfDivisors && possibleAmicablePair.Number == SumOfDivisors) && Number != possibleAmicablePair.Number)
+            {
+                AmicablePair = possibleAmicablePair;
+                IsAmicable = true;
+            }
+        }
+
+        public void reset()
+        {
+            Number = 0;
+            Divisors.Clear();
+            SumOfDivisors = 0;
+            IsAmicable = false;
+            AmicablePair = null;
+        }
+        
+        public FactorNumber()
+        {
+
+        }
+    }
+}
---
+++ ProjectEuler21/Program.cs
@@ -0,0 +1,24 @@
+﻿using ProjectEuler21;
+//Project Euler 21
+//Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+//If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+//For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+//Evaluate the sum of all the amicable numbers under 10000.
+
+List<int> sumOfAmicableNumbers = new List<int>();
+for (int x = 1; x <= 10000; x++)
+{
+    FactorNumber y = new FactorNumber();
+    y.reset();
+    y.Number = x;
+    y.GetDivisors();
+    if (y.IsAmicable)
+    {
+        Console.WriteLine(y.Number + "'s proper divisor's sum is " + y.SumOfDivisors);
+        Console.WriteLine(y.Number + " has an amicable pair with " + y.AmicablePair.Number);
+        sumOfAmicableNumbers.Add(y.Number);
+        Console.WriteLine("Current sum of amicable numbers: " + sumOfAmicableNumbers.Sum());
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Answer: "+ sumOfAmicableNumbers.Sum());
